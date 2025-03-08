49ff7cafed4b7cd05773ddf9259bd175bbfa85bb
ojiiis <official.ojingirisamuel@gmail.com>
2025-03-08 14:20:45 +0100

    1

--- loader.js
+++ loader.js
@@ -44,12 +44,21 @@ var content = "index.jl";
 switch(window.location.href.toString().split("/")[window.location.href.toString().split("/").length - 1].split("?")[0]){
     case "dashboard.html":
         content = "dashboard.jl";
+        if(window.localStorage.getItem("token") == null || window.localStorage.getItem("token") == ""){
+            window.location = "./";
+        }
     break;
     case "contact.html":
         content = "contact.jl";
     break;
     case "about.html":
         content = "about.jl";
+    break;
+    case "login.html":
+        content = "login.jl";
+    break;
+    case "registration.html":
+        content = "registration.jl";
     break;
 
 }
@@ -71,3 +80,16 @@ window.onload = () => {
         
     },2000);
 };
+
+
+async function handleForm(elem,e){
+    e.preventDefault();
+    const data = new FormData(elem);
+    const payload = {};
+    data.forEach((v,k)=>{
+        payload[k] = v;
+    });
+    const req = await fetch(elem.action,{method:"POST",headers:{"token":window.localStorage.getItem("token")},body:JSON.stringify(payload)});
+    const res = await req.json();
+    console.log(res);
+}
