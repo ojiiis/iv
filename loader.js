00814be0a2170098ab65602326f67327217daddb
ojiiis <official.ojingirisamuel@gmail.com>
2025-03-10 16:15:58 +0100

    G

--- loader.js
+++ loader.js
@@ -61,7 +61,7 @@ var content = "index.jl";
 switch(window.location.href.toString().split("/")[window.location.href.toString().split("/").length - 1].split("?")[0]){
     case "dashboard.html":
         content = "dashboard.jl";
-        alert("dashboard block");
+       // alert("dashboard block");
         if(window.localStorage.getItem("token") == null || window.localStorage.getItem("token") == ""){
             window.location = "./";
         }
@@ -88,3 +88,28 @@ App.load("./app/head.jl", "head")
    .load("./app/"+content,"body")
    .script("./app/script.jl")
    .finally(() => alert("All content loaded"));
+   
+  
+ 
+
+async function handleForm(elem,e){
+    e.preventDefault();
+    const data = new FormData(elem);
+    const payload = {};
+    data.forEach((v,k)=>{
+        payload[k] = v;
+    });
+    
+    const req = await fetch(elem.action,{method:"POST",headers:{"token":window.localStorage.getItem("token")},body:JSON.stringify(payload)});
+    const res = await req.json();
+    if(res.status){
+        if(elem.action.includes("signup")){
+            localStorage.setItem("token",res.token);
+            alert(res.message);
+            window.location.href = "./dashboard.html";
+        }
+    }else{
+        alert(res.message);
+    }
+    console.log(res);
+}
