9a39c854f9b53a034a328aa78756d864ef86c090
ojiiis <official.ojingirisamuel@gmail.com>
2025-03-12 12:19:27 +0100

    only God

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ async function handleForm(elem,e){
     
     const req = await fetch(elem.action,{method:"POST",headers:{"token":window.localStorage.getItem("token")},body:JSON.stringify(payload)});
     const res = await req.json();
+    console.log(res);
     if(res.status){
-        if(elem.action.includes("signup")){
-            localStorage.setItem("token",res.token);
-            alert(res.message);
+        if(elem.action.includes("signup") || elem.action.includes("signin")){
+            window.localStorage.setItem("token",res.token);
             window.location.href = "./dashboard.html";
+            window.localStorage.setItem("balance",JSON.stringify(res.balance));
         }
+        alert(res.message);
     }else{
         alert(res.message);
     }
     console.log(res);
+}
+
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
